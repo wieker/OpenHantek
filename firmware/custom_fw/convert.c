fc85cf971c0b224adadcd1b272e7cb2ad73b04d2
wieker <dfdfdf@gmail.com>
2017-01-02 17:31:24 +0400

    stable echo

--- firmware/custom_fw/convert.c
+++ firmware/custom_fw/convert.c
@@ -32,6 +32,8 @@ BYTE set_samplerate(BYTE rate);
 void initEps() {
     CPUCS=0x10;                  // 48 MHz, CLKOUT output disabled.
     IFCONFIG=0xc0;   SYNCDELAY;  // Internal IFCLK, 48MHz; A,B as normal ports.
+    REVCTL=0x03;     SYNCDELAY;  // See TRM...
+    EP6CFG=0xe2;     SYNCDELAY;  // 1110 0010 (bulk IN, 512 bytes, double-buffered)
     EP2CFG=0xa2;     SYNCDELAY;  // 1010 0010 (bulk OUT, 512 bytes, double-buffered)
     FIFORESET=0x80;  SYNCDELAY;  // NAK all requests from host.
     FIFORESET=0x82;  SYNCDELAY;  // Reset individual EP (2,4,6,8)
@@ -44,13 +46,13 @@ void initEps() {
     OUTPKTEND=0x82;  SYNCDELAY;  // ...(double-buffered) (required!).
 }
 
-static int ProcessSendData(void)
+static void ProcessSendData(void)
 {
     xdata const unsigned char *src=EP2FIFOBUF;
     xdata unsigned char *dest=EP6FIFOBUF;
     unsigned int len = ((int)EP2BCH)<<8 | EP2BCL;
     unsigned int i;
-    for(i=0; i<len; i++,src,dest++)
+    for(i=0; i<len; i++,src++,dest++)
     {
         if(*src>='a' && *src<='z')
         {  *dest=*src-'a'+'A';  }
@@ -58,20 +60,13 @@ static int ProcessSendData(void)
         {  *dest=*src;  }
     }
 
-    if (*src == 'A') {
-        return 0x0A;
-    }
-    if (*src == 'B') {
-        return 0x0B;
-    }
-
     // "Skip" the received OUT packet to "forget" it (see TRM p. 9-26):
     SYNCDELAY;  OUTPKTEND=0x82;
+
     // Arm the endpoint. Be sure to set BCH before BCL because BCL access
     // actually arms the endpoint.
     SYNCDELAY;  EP6BCH=len>>8;
     SYNCDELAY;  EP6BCL=len&0xff;
-    return 0;
 }
 
 #define printf(...)
@@ -319,26 +314,16 @@ void main(void) {
     int state = 0;
 
     initEps();
-    stop_sampling();
-    set_voltage(0, 1);
-    set_voltage(1, 1);
-    set_samplerate(1);
-    set_numchannels(2);
-    select_interface(0);
 
     for(;;)
     {
         // Wait for input on EP2 (EP2 non-empty).
-        if(!(EP2CS & (1<<2))) {
+        if(!(EP2CS & (1<<2)))
+        {
             // Wait for EP6 buffer to become non-full so that we don't
             // overwrite content.
-            state = ProcessSendData();
-
-            if (state == 0x0B) {
-                start_sampling();
-            } else if (state == 0x0A) {
-                stop_sampling();
-            }
+            while(EP6CS & (1<<3));
+            ProcessSendData();
         }
     }
 }
